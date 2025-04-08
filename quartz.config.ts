84e741c3f5058a1286c33640c0547f35ec144584
alisherj <bifurcatedd@gmail.com>
2025-04-08 13:52:27 -0500

    googletag

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google", tagId: 'G-84DTMV9GPC'
     },
     locale: "en-US",
     baseUrl: "alisher.site",
